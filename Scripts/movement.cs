f9e0f37ab3aa5a4d3a287f9c44448a5d12eb83e6
cmcwatta <74875036+cmcwatta@users.noreply.github.com>
2021-03-15 18:29:49 -0400

    Add files via upload

---
+++ Scripts/movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed = 5f;
+    public float rotatespeed = 5f;
+    // Update is called once per frame
+    
+
+
+    void Update()
+    {
+        float hori;
+        float vert;
+        hori = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+
+        Vector2 newVelocity = new Vector2(hori, vert);
+
+        GetComponent<Rigidbody2D>().velocity = newVelocity * speed;
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -2;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 2;
+        }
+        transform.localScale = characterScale;
+
+        
+
+         void Rotation() 
+        { 
+        float rotation = vert * rotatespeed;
+        transform.Rotate(Vector3.forward * rotation);
+        
+        }
+    }
+}
