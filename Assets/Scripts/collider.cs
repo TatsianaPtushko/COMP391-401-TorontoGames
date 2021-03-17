1a7553827424e13b34f4ce4147749a71c910da8e
Unknown <aveycharles02@gmail.com>
2021-03-17 14:44:28 -0400

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/collider.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collider : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+
+    }
+
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed = 5f;
+    public float rotatespeed = 5f;
+    // Update is called once per frame
+    
+
+
+    void Update()
+    {
+        float hori;
+        float vert;
+        hori = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+
+        Vector2 newVelocity = new Vector2(hori, vert);
+
+        GetComponent<Rigidbody2D>().velocity = newVelocity * speed;
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -1;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 1;
+        }
+        transform.localScale = characterScale;
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            characterScale.y = 1;
+        }
+        transform.localScale = characterScale;
+
+        void Rotation() 
+        { 
+        float rotation = vert * rotatespeed;
+        transform.Rotate(Vector3.forward * rotation);  
+        }
+    }
+}
